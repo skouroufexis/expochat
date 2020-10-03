0ef052640eafc147361f7d8990a53478132189cc
skouroufexis <skouroufexis@gmail.com>
2020-10-03 17:56:54 +0300

    Update App.js
    Impoorted components and added Navigator

--- App.js
+++ App.js
@@ -1,21 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput,Switch } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+import Start from './components/Start';
+import Chat from './components/Chat';
+import { render } from 'react-dom';
+const Stack = createStackNavigator();
+export default class App extends React.Component {
+  constructor(props){
+    super(props)
+   this.state={colour:'black',
+               username:'Stavros'
+          } 
+  }
+  render(){
+    return (
+      <NavigationContainer >
+        
+        <Stack.Navigator 
+          initialRouteName="Start"
+        >
+            <Stack.Screen
+              name="Start"
+              
+              component={Start}
+              setColour={this.setColour}
+            />
+            <Stack.Screen
+              name="Chat"
+              component={Chat}
+            />
+        </Stack.Navigator>
+        
+      </NavigationContainer>
+
+      // <View style={styles.container} >
+      // <Start   />
+      // {/* <Chat name={this.state.setName} colour={this.state.colour} /> */}
+      // </View>
+    );
+  }
+
+  setColour=(colour)=>{
+    this.setState({colour:colour});
+    alert(colour);
+  }
+  
+  setName=(name)=>{
+    this.setState({name:name});
+  }
+
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
