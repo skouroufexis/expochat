5e936897d1789a6b08d2b775dd6729f6c6f8c544
skouroufexis <skouroufexis@gmail.com>
2020-11-04 15:01:38 +0200

    added jsdoc  comments

--- App.js
+++ App.js
@@ -1,21 +1,39 @@
 import { StatusBar } from 'expo-status-bar';
+
+/**
+* Importing dependencies
+*  -Stack Navigator from React Navigation ((https://reactnavigation.org)  to enable navigation from one screen to another 
+* -the application components: Start.js and Chat.js
+*/
+
+
 import React from 'react';
 import { StyleSheet, Text, View, TextInput,Switch } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-
-
 import Start from './components/Start';
 import Chat from './components/Chat';
 import { render } from 'react-dom';
+
+/**
+* Initializing the React Navigation (Stack Navigator)
+*/
 const Stack = createStackNavigator();
+
+
 export default class App extends React.Component {
   constructor(props){
     super(props)
    this.state={colour:'black',
                username:'Stavros'
+
           } 
   }
+
+/**
+* renders the App
+@returns {component} NavigationContainer
+*/
   render(){
     return (
       <NavigationContainer >
@@ -25,7 +43,6 @@ export default class App extends React.Component {
         >
             <Stack.Screen
               name="Start"
-              
               component={Start}
               setColour={this.setColour}
             />
@@ -36,24 +53,28 @@ export default class App extends React.Component {
         </Stack.Navigator>
         
       </NavigationContainer>
-
-      // <View style={styles.container} >
-      // <Start   />
-      // {/* <Chat name={this.state.setName} colour={this.state.colour} /> */}
-      // </View>
     );
   }
 
+
+
+/** 
+ * function for setting the background colour of the chat screen (Chat.js component)
+ * @function setColour
+* @param {string} colour
+* sets the colour to the State
+*/
   setColour=(colour)=>{
-    this.setState({colour:colour});
-    alert(colour);
+    this.setState({colour:colour}); 
   }
   
+/** 
+ * function for setting the username (Chat.js component)
+* @function setName
+* @param {string} name
+* sets the username to the State
+*/
   setName=(name)=>{
     this.setState({name:name});
   }
-
-
 }
-
-
